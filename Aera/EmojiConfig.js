53b69e9f6c8dc4601adfff0871c8e52aae58a7e4
Yuneth Withanachchi <yunethwithanachchi03@gmail.com>
2025-10-31 21:32:10 +0530

    for the attendance

--- Aera/EmojiConfig.js
+++ Aera/EmojiConfig.js
@@ -1,2 +1,13 @@
 import 'emoji-mart'
 
+document.querySelector('emoji-picker').addEventListener('emoji-click',(event)=>{
+   const emoji = event.detail.unicode;
+   const cursorPos - input.selectionStart;
+   const textBefore = input.value.substring(0,cursorPos);
+   const textAfter = input.value.substring(cursorPos);
+   input.value = textBefore+ emoji+textAfter;
+   input.focus();
+   input.focus();
+});
+
+}
