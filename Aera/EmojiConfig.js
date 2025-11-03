2dbef30e85e3bfed291dd53a8815992222283e7a
Yuneth Withanachchi <yunethwithanachchi03@gmail.com>
2025-11-03 12:55:22 +0530

    emojis fine

--- Aera/EmojiConfig.js
+++ Aera/EmojiConfig.js
@@ -2,11 +2,21 @@ import 'emoji-picker-element'
 
 document.querySelector('emoji-picker').addEventListener('emoji-click',(event)=>{
    const emoji = event.detail.unicode;
-   const cursorPos = input.selectionStart;
-   const textBefore = input.value.substring(0,cursorPos);
-   const textAfter = input.value.substring(cursorPos);
-   input.value = textBefore+ emoji+textAfter;
-   input.focus();
+   insertEmojiAtCursor(emoji);
 
 });
 
+function insertEmojiAtCursor(emoji){
+   const sel = window.getSelection();
+   if(!sel || !sel.rangeCount) return;
+
+   const range = sel.getRangeAt(0);
+   range.deleteContents();
+   const textNode = document.createTextNode(emoji);
+   range.insertNode(textNode);
+
+   range.setStart(textNode);
+   range.setEndAfter(textNode)
+   sel.removeAllRanges();
+   sel.addRange(range);
+}
