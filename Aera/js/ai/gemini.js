acd8313084bba67c54d90d6962279b478fa80a9d
Yuneth Withanachchi <yunethwithanachchi03@gmail.com>
2026-03-26 21:51:28 +0530

    gemini api key updated

--- Aera/js/ai/gemini.js
+++ Aera/js/ai/gemini.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = "AIzaSyDij2fnmW2L7e6y8doDXkiUfsI_iMsQquc";
+const GEMINI_API_KEY = "AIzaSyDZRfd440zFJJScNnUVECJnC4HzLMzJhmg";
 
 export async function geminiReply(userMsg) {
     const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
